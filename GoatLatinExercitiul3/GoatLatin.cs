61f2de65a0097902fd91c357b2c1e9e45f5b482a
Birnat Ieronim <Ieronimb@outlook.com>
2019-07-06 11:09:10 +0300

    added exercise 3

---
+++ GoatLatinExercitiul3/GoatLatin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoatLatinExercitiul3
+{
+    class SpecialString
+    {
+        public static string GoatLatin(string str)
+        {
+            string output = string.Empty;
+            var splitString = str.Split(' ');
+
+            for (int i = 0; i < splitString.Length; i++)
+            {
+                string word = splitString[i];
+                if (word.ToLower()[0].Equals('a') || word.ToLower()[0].Equals('e') ||
+                   word.ToLower()[0].Equals('i') || word.ToLower()[0].Equals('o') ||
+                   word.ToLower()[0].Equals('u'))
+                    output += word + "ma" + new string('a', i + 1) + " ";
+                else
+                {
+                    output += (word + word[0] + "ma" + new string('a', i + 1) + " ").Remove(0, 1);
+                }
+
+            }
+            return output.Trim();
+        }
+    }
+}
---
+++ GoatLatinExercitiul3/GoatLatinUniTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoatLatinExercitiul3
+{
+    [TestFixture]
+    class GoatLatinUniTest
+    {
+       [Test]
+       public void GoatLatinTest1()
+        {
+            SpecialString  str = new SpecialString();
+            string input = "I speak Goat Latin";
+           
+            //Act
+            string output = SpecialString.GoatLatin(input);
+
+            //Asert
+            Assert.AreEqual("Imaa peaksmaaa oatGmaaaa atinLmaaaaa", output);
+        }
+
+        [Test]
+        public void GoatLatinTest2()
+        {
+            SpecialString str = new SpecialString();
+            string input = "I speak Goat Latin";
+
+
+            //Act
+            string output = SpecialString.GoatLatin(input);
+
+            //Asert
+            Assert.AreEqual("Imaa peaksmaaa oatGmaaaa atinLmaaaaa", output);
+        }
+    }
+}
